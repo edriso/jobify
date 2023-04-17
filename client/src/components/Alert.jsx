79b2309cf144411c1855e790bebbfd0e34f3e8bd
edriso <edriso.dev@gmail.com>
2023-04-18 00:59:03 +0200

    get props from AppContext

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,4 +1,7 @@
-const Alert = ({ alertType, alertText }) => {
+import { useAppContext } from '../context/appContext';
+
+const Alert = () => {
+  const { alertType, alertText } = useAppContext();
   return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 };
 export default Alert;
