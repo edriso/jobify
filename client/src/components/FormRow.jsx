30920337d832053af3ca3a00cd5f842a6bf3b199
edriso <edriso.dev@gmail.com>
2023-04-17 21:34:35 +0200

    add name attribute

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,12 +1,13 @@
-function FormRow({ type = 'text', name, value, onChange, labelText }) {
+function FormRow({ type = 'text', name, value, onChange, labelText = name }) {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
-        {labelText || name}
+        {labelText}
       </label>
       <input
         type={type}
         value={value}
+        name={name}
         onChange={onChange}
         className="form-input"
         id={name}
