a8e31e0e8a55d4b3a483991ea9c526d9d458356c
edriso <edriso.dev@gmail.com>
2023-04-17 07:28:57 +0200

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Landing } from './pages';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>dashboard</div>} />
+        <Route path="/register" element={<div>register</div>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
