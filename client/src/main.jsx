80ba539d9a8e9a0524f05485e98408091989a176
edriso <edriso.dev@gmail.com>
2023-04-17 02:28:37 +0200

    add normalize.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'normalize.css';
 import './assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
