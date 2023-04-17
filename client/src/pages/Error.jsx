1548a96788093f91265fb16a265ee8bbffebe057
edriso <edriso.dev@gmail.com>
2023-04-17 19:20:06 +0200

    code error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,17 @@
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/styledWrappers/ErrorPage';
+import notFound from '../assets/images/not-found.svg';
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={notFound} alt="not found" />
+        <h3>Page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">Back to home</Link>
+      </div>
+    </Wrapper>
+  );
 }
 export default Error;
