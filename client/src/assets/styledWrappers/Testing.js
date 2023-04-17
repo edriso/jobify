3bfad9c2c09df48fe6e5d905c2fd6c6dc376760a
edriso <edriso.dev@gmail.com>
2023-04-17 06:07:38 +0200

    rename styledWrappers folder

--- client/src/assets/styledWrappers/Testing.js
+++ client/src/assets/styledWrappers/Testing.js
@@ -1,29 +1,33 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.main`
+const StyledMain = styled.main`
   nav {
     width: var(--fluid-width);
     max-width: var(--max-width);
-    margin: 0 auto;
     height: var(--nav-height);
+    margin: 0 auto;
     display: flex;
     align-items: center;
   }
+
   .page {
     min-height: calc(100vh - var(--nav-height));
     display: grid;
     align-items: center;
     margin-top: -3rem;
   }
+
   h1 {
     font-weight: 700;
     span {
       color: var(--primary-500);
     }
   }
+
   p {
     color: var(--grey-600);
   }
+
   .main-img {
     display: none;
   }
@@ -32,9 +36,11 @@ const Wrapper = styled.main`
       grid-template-columns: 1fr 1fr;
       column-gap: 3rem;
     }
+
     .main-img {
       display: block;
     }
   }
 `;
-export default Wrapper;
+
+export default StyledMain;
