b4894712e68a8be1626cf79963c6b1e923d3d1cc
edriso <edriso.dev@gmail.com>
2023-04-18 13:47:12 +0200

    connect to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './db/connect.js';
 // middleware
 import notFoundMiddleware from './middleware/notFound.js';
 import errorHandlerMiddleware from './middleware/errorHandler.js';
@@ -15,4 +16,14 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}...`));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
