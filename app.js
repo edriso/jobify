f8c983ab7235c422743dde833441886b7896cd16
edriso <edriso.dev@gmail.com>
2023-04-18 14:50:07 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+// routers
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
+// middleware
+import notFoundMiddleware from './middleware/notFound.js';
+import errorHandlerMiddleware from './middleware/errorHandler.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/v1', (req, res) => {
+  res.send('<h1>Jobify API!</h1>');
+});
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+export default app;
--- server.js
+++ server.js
@@ -1,34 +1,16 @@
 import dotenv from 'dotenv';
-import express from 'express';
-// db and authenticateUser
-import connectDB from './db/connect.js';
-// routers
-import authRouter from './routes/authRoutes.js';
-import jobsRouter from './routes/jobsRoutes.js';
-// middleware
-import notFoundMiddleware from './middleware/notFound.js';
-import errorHandlerMiddleware from './middleware/errorHandler.js';
-
 dotenv.config();
-const app = express();
 
-app.use(express.json());
-
-app.get('/api/v1', (req, res) => {
-  res.send('<h1>Jobify API!</h1>');
-});
-
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
-
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+import connectDB from './db/connect.js';
+import app from './app.js';
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URL).then(() =>
+      console.log('Connected to DB...')
+    );
     app.listen(port, () => console.log(`Server is running on port ${port}...`));
   } catch (error) {
     console.log(error);
