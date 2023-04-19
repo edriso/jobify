43df6b7d6e0400fbdd0a9e15716385c17c43ed84
edriso <edriso.dev@gmail.com>
2023-04-20 01:53:38 +0200

    add jtw lifetime to .env

--- models/User.js
+++ models/User.js
@@ -46,7 +46,7 @@ userSchema.pre('save', async function () {
 
 userSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
+    expiresIn: process.env.JWT_LIFETIME || '1d',
   });
 };
 
