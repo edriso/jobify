b45934b7b6afab9a1e8abd6cb8fb0e3d27e7a6a9
edriso <edriso.dev@gmail.com>
2023-04-19 15:05:41 +0200

    handle unique field error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -11,6 +11,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .join(', ');
   }
 
+  if (err.code && err.code === 11000) {
+    defaultError.status = 400;
+    defaultError.message = `${Object.keys(err.keyValue)} is already in use`;
+  }
+
   // res.status(defaultError.status).json({ message: err });
   res.status(defaultError.status).json({ message: defaultError.message });
 };
