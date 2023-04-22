033afb87d5cdabc636e3ea23e9100715e89f0af5
edriso <edriso.dev@gmail.com>
2023-04-22 11:15:21 +0200

    get port dynamically

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: `http://localhost:${process.env.PORT || 5000}`,
         changeOrigin: true,
       },
     },
