c95e82b0d5a89ffde46a7fde33df7a338edb82c6
edriso <edriso.dev@gmail.com>
2023-04-22 22:05:52 +0200

    add user to localStorage

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -9,14 +9,19 @@ import {
 } from './actions';
 import axios from 'axios';
 
+const user = localStorage.getItem('user');
+const token = localStorage.getItem('token');
+const userLocation = localStorage.getItem('location');
+
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: '',
   alertType: '',
-  user: null,
-  token: null,
-  userLocation: '',
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || '',
+  jobLocation: userLocation || '',
 };
 
 const AppContext = createContext();
@@ -35,13 +40,25 @@ function AppProvider({ children }) {
     }, 3000);
   };
 
+  const addUserToLocalStorage = (user, token, location) => {
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+    localStorage.setItem('location', location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('location');
+  };
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
 
     try {
       const response = await axios.post('/api/v1/auth/register', currentUser);
-      console.log(response);
       const { user, token } = response.data;
+
       dispatch({
         type: REGISTER_USER_SUCCESS,
         payload: {
@@ -51,7 +68,7 @@ function AppProvider({ children }) {
         },
       });
 
-      // localStorage later
+      addUserToLocalStorage({ user, token, location: user.location });
     } catch (error) {
       dispatch({
         type: REGISTER_USER_ERROR,
