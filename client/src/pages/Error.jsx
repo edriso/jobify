c4625dc5a57d3ce90ebac6f92c8b40c0187c8b3a
edriso <edriso.dev@gmail.com>
2023-10-11 13:44:07 +0300

    update Error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,21 +1,26 @@
-import {
-  Link,
-  // useRouteError
-} from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 import Wrapper from '../assets/styledWrappers/ErrorPage';
 import notFound from '../assets/images/not-found.svg';
 
 function Error() {
-  // const error = useRouteError();
-  // console.log(error);
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper className="full-page">
+        <div>
+          <img src={notFound} alt="not found" />
+          <h3>Page not found</h3>
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to="/dashboard">Back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <Wrapper className="full-page">
-      <div>
-        <img src={notFound} alt="not found" />
-        <h3>Page not found</h3>
-        <p>We can't seem to find the page you're looking for</p>
-        <Link to="/dashboard">Back home</Link>
-      </div>
+    <Wrapper>
+      <h3>Something went wrong</h3>
     </Wrapper>
   );
 }
