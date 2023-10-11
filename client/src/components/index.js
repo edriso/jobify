1806659a490759f77c4f9ce7334a8f468e46da1e
edriso <edriso.dev@gmail.com>
2023-10-11 13:52:12 +0300

    add SubmitBtn component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Logo } from './Logo';
 export { default as FormRow } from './FormRow';
-export { default as Alert } from './Alert';
+export { default as SubmitBtn } from './SubmitBtn';
