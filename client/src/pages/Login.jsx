6137223ca8ac24a30831cf195c8c5c4108ddd94f
edriso <edriso.dev@gmail.com>
2023-10-11 13:52:21 +0300

    add Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,27 @@
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/styledWrappers/RegisterAndLoginPage';
+import { FormRow, Logo, SubmitBtn } from '../components';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Wrapper>
+      <form method="post" className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
+        <SubmitBtn />
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
 export default Login;
