151b874bc79a069f0ccb85f28bbb7708bea1af15
edriso <edriso.dev@gmail.com>
2023-10-14 19:11:07 +0300

    add navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import Wrapper from '../assets/styledWrappers/Navbar';
+import { FaAlignLeft } from 'react-icons/fa';
+import Logo from './Logo';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 function Navbar() {
-  return <Wrapper>Navbar</Wrapper>;
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button className="toggle-btn" type="button" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">Dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Navbar;
