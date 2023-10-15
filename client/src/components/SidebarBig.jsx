a00801d691d0ffba09cbeddff94ace7daef79ea6
edriso <edriso.dev@gmail.com>
2023-10-15 15:19:19 +0300

    add SidebarBig structure and functionality

--- client/src/components/SidebarBig.jsx
+++ client/src/components/SidebarBig.jsx
@@ -1,7 +1,30 @@
 import Wrapper from '../assets/styledWrappers/SidebarBig';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 
 function SidebarBig() {
-  return <Wrapper>SidebarBig</Wrapper>;
+  const { showSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          // showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+          // The logic is flipped as I want to show the sidebar by default, but hide on small screens
+          // remember that showSidebar initially is false
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default SidebarBig;
