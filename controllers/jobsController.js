2650470b657e8bc4b22b17ee1832fbc3f61eecfa
edriso <edriso.dev@gmail.com>
2023-10-18 19:43:56 +0300

    use crypto instead of nanoid

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,21 +1,28 @@
+import crypto from 'crypto';
+
 const getAllJobs = async (req, res) => {
-  res.status(200).send('getAllJobs');
+  let jobs = [
+    { id: crypto.randomUUID(), company: 'google', position: 'backend' },
+    { id: crypto.randomUUID(), company: 'apple', position: 'frontend' },
+  ];
+
+  res.status(200).json(jobs);
 };
 
 const createJob = async (req, res) => {
-  res.status(201).send('createJob');
+  res.status(201).json('createJob');
 };
 
 const updateJob = async (req, res) => {
-  res.status(200).send('updateJob');
+  res.status(200).json('updateJob');
 };
 
 const deleteJob = async (req, res) => {
-  res.status(204).send('deleteJob');
+  res.status(204).json('deleteJob');
 };
 
 const showStats = async (req, res) => {
-  res.status(200).send('showStats');
+  res.status(200).json('showStats');
 };
 
 export default { getAllJobs, createJob, updateJob, deleteJob, showStats };
