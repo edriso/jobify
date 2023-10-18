86c24685c78eee272870276e9bc10a9cc2719acb
edriso <edriso.dev@gmail.com>
2023-10-18 19:41:02 +0300

    fix import error

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import {
-  getAllJobs,
-  createJob,
-  updateJob,
-  deleteJob,
-  showStats,
-} from '../controllers/jobsController.js';
+import jobsController from '../controllers/jobsController.js';
 
 const router = express.Router();
 
-router.route('/').get(getAllJobs).post(createJob);
-
-router.route('/stats').get(showStats);
-
-router.route('/:id').patch(updateJob).delete(deleteJob);
+router.route('/').get(jobsController.getAllJobs).post(jobsController.createJob);
+router.route('/stats').get(jobsController.showStats);
+router
+  .route('/:id')
+  .patch(jobsController.updateJob)
+  .delete(jobsController.deleteJob);
 
 export default router;
