66e3015b2416a7c39b66a925cc06d45ead1f422e
edriso <edriso.dev@gmail.com>
2023-10-18 19:01:47 +0300

    update import object

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,15 +1,18 @@
 import express from 'express';
-import jobsController from '../controllers/jobsController.js';
+import {
+  getAllJobs,
+  createJob,
+  updateJob,
+  deleteJob,
+  showStats,
+} from '../controllers/jobsController.js';
 
 const router = express.Router();
 
-router.route('/').get(jobsController.getAllJobs).post(jobsController.createJob);
+router.route('/').get(getAllJobs).post(createJob);
 
-router.route('/stats').get(jobsController.showStats);
+router.route('/stats').get(showStats);
 
-router
-  .route('/:id')
-  .patch(jobsController.updateJob)
-  .delete(jobsController.deleteJob);
+router.route('/:id').patch(updateJob).delete(deleteJob);
 
 export default router;
