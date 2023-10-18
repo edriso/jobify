2684ed93cf565919fc7c5f458c689db2745265e9
edriso <edriso.dev@gmail.com>
2023-10-18 19:39:51 +0300

    add Job model

---
+++ models/Job.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+
+const jobSchema = new mongoose.Schema(
+  {
+    company: String,
+    position: String,
+    jobStatus: {
+      type: String,
+      enum: Object.values(JOB_STATUS),
+      default: JOB_STATUS.PENDING,
+    },
+    jobType: {
+      type: String,
+      enum: Object.values(JOB_TYPE),
+      default: JOB_TYPE.FULL_TIME,
+    },
+    jobLocation: {
+      type: String,
+      default: 'my city',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+const Job = mongoose.model('Job', jobSchema);
+export default Job;
