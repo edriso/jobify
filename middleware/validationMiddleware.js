ed0aa275a5e4bc304641f4a786405fd0951e7734
edriso <edriso.dev@gmail.com>
2023-10-21 16:00:55 +0300

    comment out validateJobIdAndCheckExistence

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,8 +1,7 @@
 import { body, param, validationResult } from 'express-validator';
 import mongoose from 'mongoose';
-import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
+import { BadRequestError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
-import Job from '../models/jobModel.js';
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -11,9 +10,9 @@ const withValidationErrors = (validateValues) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg);
-        if (errorMessages[0].startsWith('no job')) {
-          throw new NotFoundError(errorMessages);
-        }
+        // if (errorMessages[0].startsWith('no job')) {
+        //   throw new NotFoundError(errorMessages);
+        // }
         throw new BadRequestError(errorMessages);
       }
       next();
@@ -43,14 +42,15 @@ export const validateJobInput = withValidationErrors([
     .withMessage('invalid type value'),
 ]);
 
-export const validateJobIdAndCheckExistence = withValidationErrors([
-  param('id').custom(async (value) => {
-    const isValidId = mongoose.Types.ObjectId.isValid(value);
-    if (!isValidId) throw new BadRequestError('invalid MongoDB id');
-    const job = await Job.findById(value);
-    if (!job) throw new NotFoundError(`no job with id ${value}`);
-  }),
-]);
+//// Added to a separate middleware
+// export const validateJobIdAndCheckExistence = withValidationErrors([
+//   param('id').custom(async (value) => {
+//     const isValidId = mongoose.Types.ObjectId.isValid(value);
+//     if (!isValidId) throw new Error('invalid MongoDB id');
+//     const job = await Job.findById(value);
+//     if (!job) throw new Error(`no job with id ${value}`);
+//   }),
+// ]);
 
 export const validateIdParam = withValidationErrors([
   param('id')
