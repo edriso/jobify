5d6bb600c6384fd07287157aa596486ef441307d
edriso <edriso.dev@gmail.com>
2023-10-21 13:01:30 +0300

    rename validateJobInput

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,5 +1,6 @@
 import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/customErrors.js';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -7,8 +8,10 @@ const withValidationErrors = (validateValues) => {
     (req, res, next) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map((error) => error.msg);
-        // .join(', ');
+        const errorMessages = errors
+          .array()
+          .map((error) => error.msg)
+          .join(', ');
         throw new BadRequestError(errorMessages);
       }
       next();
@@ -16,7 +19,7 @@ const withValidationErrors = (validateValues) => {
   ];
 };
 
-export const validateJob = withValidationErrors([
+export const validateJobInput = withValidationErrors([
   body('company')
     .notEmpty()
     .withMessage('company is required')
@@ -29,4 +32,11 @@ export const validateJob = withValidationErrors([
     .isLength({ max: 50 })
     .withMessage('position should be at less than 50 chars')
     .trim(),
+  body('jobLocation').notEmpty().withMessage('jobLocation is required'),
+  body('jobStatus')
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage('invalid status value'),
+  body('jobType')
+    .isIn(Object.values(JOB_TYPE))
+    .withMessage('invalid type value'),
 ]);
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import jobController from '../controllers/jobController.js';
-import { validateJob } from '../middleware/validationMiddleware.js';
+import { validateJobInput } from '../middleware/validationMiddleware.js';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(jobController.getAllJobs)
-  .post(validateJob, jobController.createJob);
+  .post(validateJobInput, jobController.createJob);
 router.route('/stats').get(jobController.showStats);
 router
   .route('/:id')
   .get(jobController.getJob)
-  .patch(validateJob, jobController.updateJob)
+  .patch(validateJobInput, jobController.updateJob)
   .delete(jobController.deleteJob);
 
 export default router;
