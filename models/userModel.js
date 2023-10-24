84b38d23d3b0bc6cb74774d003f47cd90de36015
edriso <edriso.dev@gmail.com>
2023-10-24 13:47:47 +0300

    update userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,42 +1,14 @@
 import mongoose from 'mongoose';
-import validator from 'validator';
-import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+// import validator from 'validator';
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-    minlength: 3,
-    maxlength: 20,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    trim: true,
-    validate: {
-      validator: validator.isEmail,
-      message: 'Invalid email',
-    },
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'Password should be at least 6 characters'],
-    select: false,
-  },
-  lastName: {
-    type: String,
-    maxlength: 20,
-    trim: true,
-  },
+  name: String,
+  email: String,
+  password: String,
+  lastName: String,
   location: {
     type: String,
-    maxlength: 20,
-    trim: 'true',
-    default: 'Egypt',
+    default: 'my city',
   },
   role: {
     type: String,
@@ -44,22 +16,6 @@ const userSchema = new mongoose.Schema({
     default: 'user',
   },
 });
-
-userSchema.pre('save', async function () {
-  const salt = await bcryptjs.genSalt(10);
-  this.password = await bcryptjs.hash(this.password, salt);
-});
-
-userSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME || '1h',
-  });
-};
-
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  const isMatch = await bcryptjs.compare(candidatePassword, this.password);
-  return isMatch;
-};
 
 const User = mongoose.model('User', userSchema);
 
