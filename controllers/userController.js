675531fe594bf386ee1ef9d3104ea309dfe3658d
edriso <edriso.dev@gmail.com>
2023-10-25 16:59:40 +0300

    exclude password from req.body on update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,8 @@ const getApplicationStats = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  await User.findByIdAndUpdate(req.user.userId, req.body, { new: true });
+  delete req.body.password;
+  await User.findByIdAndUpdate(req.user.userId, req.body);
   res.status(200).json({ message: 'user updated' });
 };
 
