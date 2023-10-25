e793bdedbe17a1c0def862e1bfea3843451dc57b
edriso <edriso.dev@gmail.com>
2023-10-25 15:31:13 +0300

    update route paths

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,8 @@ import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get('/', userController.getCurrentUser);
-router.get('/stats', userController.getApplicationStats);
-router.get('/update-user', userController.updateUser);
+router.get('/current-user', userController.getCurrentUser);
+router.get('/admin/app-stats', userController.getApplicationStats);
+router.patch('/update-user', userController.updateUser);
 
 export default router;
