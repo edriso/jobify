8e266ce1c89d3774126d23074fd68e2c7c4387c1
edriso <edriso.dev@gmail.com>
2023-10-25 16:23:55 +0300

    get current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,16 @@
 import User from '../models/userModel.js';
 import Job from '../models/jobModel.js';
+import { UnauthorizedError } from '../errors/customErrors.js';
 
 const getCurrentUser = async (req, res) => {
-  res.status(200).json({ message: 'getCurrentUser' });
+  const user = await User.findById(req.user.userId);
+  res.status(200).json({ user: user.hidePassword() });
 };
 
 const getApplicationStats = async (req, res) => {
+  const isAdmin = req.user.role === 'admin';
+  if (!isAdmin)
+    throw new UnauthorizedError('not authorized to access this route');
   res.status(200).json({ message: 'getApplicationStats' });
 };
 
