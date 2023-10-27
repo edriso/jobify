1c58f13dc2bcf7a705fab82ce4f8c7bb5f5122a7
edriso <edriso.dev@gmail.com>
2023-10-27 19:22:39 +0200

    add registerAction to Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import {
   Profile,
   Admin,
 } from './pages';
+import { action as registerAction } from './pages/Register';
 
 const checkDefaultTheme = () => {
   const isDarkThemeEnabled = localStorage.getItem('darkTheme') === 'true';
@@ -33,6 +34,7 @@ const router = createBrowserRouter([
       {
         path: 'register',
         element: <Register />,
+        action: registerAction,
       },
       {
         path: 'login',
