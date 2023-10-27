3ea9eeb8dd8f259fc8191a780c16aa732ef0dcc0
edriso <edriso.dev@gmail.com>
2023-10-27 19:32:30 +0200

    complete register user

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Form, redirect, useNavigate, Link } from 'react-router-dom';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/styledWrappers/RegisterAndLoginPage';
+import apiHandler from '../utils/apiHandler';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await apiHandler.post('/auth/register', data);
+    return redirect('/login');
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 function Register() {
   return (
-    <Wrapper className="full-page">
-      <form className="form">
+    <Wrapper>
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
         <FormRow type="text" name="name" />
-        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name *"
+          optional={true}
+        />
         <FormRow type="text" name="location" />
         <FormRow type="email" name="email" />
         <FormRow type="password" name="password" />
@@ -22,8 +41,9 @@ function Register() {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 }
+
 export default Register;
