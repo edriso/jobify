13606c0dcbba0372cfdd41339ba3eedccd5c5980
edriso <edriso.dev@gmail.com>
2023-10-28 18:03:05 +0200

    use useActionData hook

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect, Link } from 'react-router-dom';
+import { Form, redirect, Link, useActionData } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Logo, FormRow, SubmitBtn } from '../components';
 import Wrapper from '../assets/styledWrappers/RegisterAndLoginPage';
@@ -7,17 +7,24 @@ import apiHandler from '../utils/apiHandler';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const errors = { message: '' };
+  if (data.password.length < 8) {
+    errors.message = 'Password must be at least 8 characters long';
+    return errors;
+  }
   try {
     await apiHandler.post('/auth/register', data);
     toast.success('Registration Successful');
     return redirect('/login');
   } catch (error) {
+    // errors.message = error?.response?.data?.message; // alternative way to display errors
     toast.error(error?.response?.data?.message);
     return error;
   }
 };
 
 function Register() {
+  const errors = useActionData();
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -33,6 +40,9 @@ function Register() {
         <FormRow type="text" name="location" />
         <FormRow type="email" name="email" />
         <FormRow type="password" name="password" />
+        {errors?.message && (
+          <p style={{ color: '#f98080' }}>{errors.message}</p>
+        )}
         <SubmitBtn />
         <p>
           Already a member?
