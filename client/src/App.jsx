7fde134e0a802934eadc20dc47c44a92ad50cce7
edriso <edriso.dev@gmail.com>
2023-10-28 17:10:40 +0200

    add loginAction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import {
   Admin,
 } from './pages';
 import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
 
 const checkDefaultTheme = () => {
   const isDarkThemeEnabled = localStorage.getItem('darkTheme') === 'true';
@@ -39,6 +40,7 @@ const router = createBrowserRouter([
       {
         path: 'login',
         element: <Login />,
+        action: loginAction,
       },
       {
         path: 'dashboard',
