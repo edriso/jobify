8f38a8a04859ee8c5f79192e5cf2f6c9eef0f66c
edriso <edriso.dev@gmail.com>
2023-10-29 22:18:01 +0200

    fix condition within className in SubmitBtn component

--- client/src/components/SubmitBtn.jsx
+++ client/src/components/SubmitBtn.jsx
@@ -7,7 +7,7 @@ function SubmitBtn({ formBtn }) {
   return (
     <button
       type="submit"
-      className={`btn btn-block ${formBtn && 'form-btn'} `}
+      className={`btn btn-block${formBtn ? ' form-btn' : ''}`}
       disabled={isSubmitting}
     >
       {isSubmitting ? 'submitting' : 'submit'}
