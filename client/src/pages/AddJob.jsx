ba749faf7d42ae82d4fc7072e215e567b4794a84
edriso <edriso.dev@gmail.com>
2023-10-29 22:53:16 +0200

    AddJob structure

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,4 +1,31 @@
+import { Form, redirect, useOutletContext } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Wrapper from '../assets/styledWrappers/DashboardFormPage';
+import apiHandler from '../utils/apiHandler';
+import { FormRow, SubmitBtn } from '../components';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+
 function AddJob() {
-  return <div>AddJob</div>;
+  const { user } = useOutletContext();
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
+  );
 }
 export default AddJob;
