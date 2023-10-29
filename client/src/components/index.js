c6b02e9d0cd4a8cbcefdee5fcd6c8b98eafdd462
edriso <edriso.dev@gmail.com>
2023-10-29 23:14:05 +0200

    export FormRowSelect

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Logo } from './Logo';
 export { default as FormRow } from './FormRow';
+export { default as FormRowSelect } from './FormRowSelect';
 export { default as SubmitBtn } from './SubmitBtn';
 export { default as SidebarBig } from './SidebarBig';
 export { default as SidebarSmall } from './SidebarSmall';
