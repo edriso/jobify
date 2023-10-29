d9cd4f8b14c37ab5840f4f924e542748bd91ca5e
edriso <edriso.dev@gmail.com>
2023-10-29 23:09:11 +0200

    add FormRowSelect component

---
+++ client/src/components/FormRowSelect.jsx
@@ -0,0 +1,26 @@
+function FormRowSelect({ name, labelText, defaultValue = '', list, onChange }) {
+  return (
+    <div className="form-row">
+      <label htmlFor="jobStatus" className="form-label">
+        {labelText || name}
+      </label>
+      <select
+        name={name}
+        id={name}
+        className="form-select"
+        defaultValue={defaultValue}
+        onChange={onChange}
+      >
+        {list.map((item) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
+
+export default FormRowSelect;
