b8fbd5bb39a8c186de48156427e833c6c405e1bd
edriso <edriso.dev@gmail.com>
2023-10-30 11:47:08 +0200

    add JobsContainer logic

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,5 +1,23 @@
+import Job from './Job';
+import Wrapper from '../assets/styledWrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+
 function JobsContainer() {
-  return <div>JobsContainer</div>;
+  const { data } = useAllJobsContext();
+
+  if (data.jobs.length === 0) {
+    return <Wrapper>No jobs to display</Wrapper>;
+  }
+
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {data.jobs.map((job) => (
+          <Job key={job._id} {...job} />
+        ))}
+      </div>
+    </Wrapper>
+  );
 }
 
 export default JobsContainer;
