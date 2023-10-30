1d6924bbdec81a1932bac3f285d4ba4b6164f7dc
edriso <edriso.dev@gmail.com>
2023-10-30 12:21:00 +0200

    complete Job component

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,5 +1,50 @@
-function Job() {
-  return <div>Job</div>;
+import { Link, Form } from 'react-router-dom';
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
+import day from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import Wrapper from '../assets/styledWrappers/Job';
+import JobInfo from './JobInfo';
+day.extend(advancedFormat);
+
+function Job({
+  company,
+  position,
+  jobLocation,
+  jobStatus,
+  jobType,
+  createdAt,
+}) {
+  const date = day(createdAt).format('MMM Do, YYYY');
+  console.log(date);
+  return (
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="i">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+
+      <div className="content">
+        <div className="content-center">
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${jobStatus}`}>{jobStatus}</div>
+        </div>
+
+        <footer className="actions">
+          <Link className="btn edit-btn">Edit</Link>
+          <Form>
+            <button type="submit" className="btn delete-btn">
+              Delete
+            </button>
+          </Form>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Job;
