02fdb000a1a0f71cd78afc6b26b4f0854d89e47d
edriso <edriso.dev@gmail.com>
2023-10-30 11:34:42 +0200

    create AllJobsContext

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -14,14 +14,17 @@ export const loader = async () => {
   }
 };
 
+const AllJobsContext = createContext();
 function AllJobs() {
   const { data } = useLoaderData();
   return (
-    <>
+    <AllJobsContext.Provider value={{ data }}>
       <SearchContainer />
       <JobsContainer />
-    </>
+    </AllJobsContext.Provider>
   );
 }
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
 
 export default AllJobs;
