d19cf80078979f2fcdf7b7a4c6d4246e20f3def6
edriso <edriso.dev@gmail.com>
2023-10-30 21:43:18 +0200

    add Admin logic

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,4 +1,26 @@
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa';
+import apiHandler from '../utils/apiHandler';
+import Wrapper from '../assets/styledWrappers/StatsContainer';
+
+export const loader = async () => {
+  try {
+    const response = await apiHandler.get('/users/admin/app-stats');
+    return response.data;
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+    return redirect('/dashboard/all-jobs');
+  }
+};
+
 function Admin() {
-  return <div>Admin</div>;
+  const { users, jobs } = useLoaderData();
+  return (
+    <Wrapper>
+      <h3>admin</h3>
+    </Wrapper>
+  );
 }
+
 export default Admin;
