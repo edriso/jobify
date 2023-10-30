c8f16b986de304eda5f00bd4caa52392cbf28cd1
edriso <edriso.dev@gmail.com>
2023-10-30 13:13:23 +0200

    add DeleteJob logic

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,5 +1,13 @@
-function DeleteJob() {
-  return <div>DeleteJob</div>;
-}
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import apiHandler from '../utils/apiHandler';
 
-export default DeleteJob;
+export const action = async ({ params }) => {
+  try {
+    await apiHandler.delete(`/jobs/${params.id}`);
+    toast.success('Job deleted successfully');
+  } catch (error) {
+    toast.error('Could not delete that job');
+  }
+  return redirect('/dashboard/all-jobs');
+};
