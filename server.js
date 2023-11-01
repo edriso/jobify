0e1b34b65c118e6e995ef5a19c3099d5b4837130
edriso <edriso.dev@gmail.com>
2023-11-01 19:33:21 +0200

    delete app.js file in backend

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
-import app from './app.js';
+import 'express-async-errors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 import connectDB from './db/connect.js';
+// routers
+import authRouter from './routes/authRouter.js';
+import jobRouter from './routes/jobRouter.js';
+import userRouter from './routes/userRouter.js';
+// middlewares
+import notFoundMiddleware from './middleware/notFoundMiddleware.js';
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
+
+const app = express();
+
+app.use(cookieParser()); // to be able to access cookies
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/api/v1', (req, res) => {
+  res.send('<h1>Jobify API!</h1>');
+});
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
+app.use('/api/v1/auth', authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
-
-const start = async () => {
+const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () => console.log(`Server is running on port ${port}...`));
@@ -12,4 +42,4 @@ const start = async () => {
   }
 };
 
-start();
+startServer();
