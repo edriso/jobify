6fb900aa6e190b60398d94d2249f7a4ca223e2a8
edriso <edriso.dev@gmail.com>
2023-11-02 11:57:54 +0200

    add multerMiddleware

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,13 +2,14 @@ import express from 'express';
 import userController from '../controllers/userController.js';
 import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
 import { authorizePermissions } from '../middleware/authMiddleware.js';
+import upload from '../middleware/multerMiddleware.js';
 
 const router = express.Router();
 
 router.get('/current-user', userController.getCurrentUser);
 router.patch(
   '/update-user',
-  [validateUpdateUserInput],
+  [upload.single('avatar'), validateUpdateUserInput],
   userController.updateUser
 );
 router.get(
