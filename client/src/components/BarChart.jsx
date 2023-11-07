cfeeb008f93d8af497adeef32f2acd9c5c03188f
edriso <edriso.dev@gmail.com>
2023-11-07 18:34:10 +0200

    add bar chart functionality

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,5 +1,25 @@
-function BarChart() {
-  return <div>BarChart</div>;
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+
+function BarChartComponent({ data }) {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 }
 
-export default BarChart;
+export default BarChartComponent;
