2e1e35cd72597be63c6559f39e80149ad3d86734
edriso <edriso.dev@gmail.com>
2023-11-07 18:11:20 +0200

    complete StatsContainer section

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,7 +1,39 @@
-import AreaChart from './AreaChart';
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
+import Wrapper from '../assets/styledWrappers/StatsContainer';
+import StatItem from './StatItem';
 
-function StatsContainer() {
-  return <div>StatsContainer</div>;
+function StatsContainer({ defaultStats }) {
+  const stats = [
+    {
+      title: 'pending applications',
+      count: defaultStats?.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: '#f59e0b',
+      bg: '#fef3c7',
+    },
+    {
+      title: 'interviews scheduled',
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: '#647acb',
+      bg: '#e0e8f9',
+    },
+    {
+      title: 'jobs declined',
+      count: defaultStats?.declined || 0,
+      icon: <FaBug />,
+      color: '#d66a6a',
+      bg: '#ffeeee',
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {stats.map((item) => {
+        return <StatItem key={item.title} {...item} />;
+      })}
+    </Wrapper>
+  );
 }
 
 export default StatsContainer;
