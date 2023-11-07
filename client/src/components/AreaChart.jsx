4fdc89a5e37ba852398f4ab6e397592194c24791
edriso <edriso.dev@gmail.com>
2023-11-07 18:30:19 +0200

    add area chart functionality

--- client/src/components/AreaChart.jsx
+++ client/src/components/AreaChart.jsx
@@ -1,5 +1,25 @@
-function AreaChart() {
-  return <div>AreaChart</div>;
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+
+function AreaChartComponent({ data }) {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#2cb1bc" fill="#bef8fd" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 }
 
-export default AreaChart;
+export default AreaChartComponent;
