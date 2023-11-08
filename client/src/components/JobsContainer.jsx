3cf71ed20652b71762d270569f252bd95f2e1a32
edriso <edriso.dev@gmail.com>
2023-11-08 19:43:21 +0200

    import PageBtnContainer

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,21 +1,29 @@
 import Job from './Job';
 import Wrapper from '../assets/styledWrappers/JobsContainer';
 import { useAllJobsContext } from '../pages/AllJobs';
+import PageBtnContainer from './PageBtnContainer';
 
 function JobsContainer() {
-  const { data } = useAllJobsContext();
+  const {
+    data: { jobs, totalJobs, numOfPages },
+  } = useAllJobsContext();
 
-  if (data.jobs.length === 0) {
+  if (jobs.length === 0) {
     return <Wrapper>No jobs to display</Wrapper>;
   }
 
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{totalJobs > 1 && 's'}
+      </h5>
       <div className="jobs">
-        {data.jobs.map((job) => (
+        {jobs.map((job) => (
           <Job key={job._id} {...job} />
         ))}
       </div>
+
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 }
