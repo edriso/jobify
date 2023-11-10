d5955a49e0f11fcc933fb235832c947b6e5228f1
edriso <edriso.dev@gmail.com>
2023-11-10 22:54:46 +0200

    save temp image on memory instead of disk

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
-import { promises as fs } from 'fs';
+// import { promises as fs } from 'fs';
 import User from '../models/userModel.js';
 import Job from '../models/jobModel.js';
 import { v2 as cloudinary } from 'cloudinary';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 const getCurrentUser = async (req, res) => {
   const user = await User.findById(req.user.userId);
@@ -12,8 +13,13 @@ const updateUser = async (req, res) => {
   const updatedUser = { ...req.body };
   delete updatedUser.password;
   if (req.file) {
-    const response = await cloudinary.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path); // deletes the local img
+    //// if image was saved on disk
+    // const response = await cloudinary.uploader.upload(req.file.path);
+    // await fs.unlink(req.file.path); // deletes the local img
+    //// if image was saved on memory
+    const file = formatImage(req.file);
+    const response = await cloudinary.uploader.upload(file);
+
     updatedUser.avatar = response.secure_url;
     updatedUser.avatarPublicId = response.public_id;
   }
