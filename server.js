7a050464c42f95ab8092032cf662a147c5ea3486
edriso <edriso.dev@gmail.com>
2023-11-10 19:34:54 +0200

    add route to dist instead of public for production app

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ cloudinary.config({
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(cookieParser()); // to be able to access cookies
 app.use(express.json());
@@ -40,7 +41,7 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use(notFoundMiddleware);
