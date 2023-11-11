b62d254c21d6626ed522f6dd11403dbba9158e11
edriso <edriso.dev@gmail.com>
2023-11-11 14:00:32 +0200

    trying react-query to cache stats data

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,25 +1,29 @@
-import { useLoaderData } from 'react-router-dom';
+// import { useLoaderData } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import { StatsContainer, ChartsContainer } from '../components';
 import apiHandler from '../utils/apiHandler';
 
 export const loader = async () => {
-  const response = await apiHandler.get('/jobs/stats');
-  return response.data;
-  // try {
-  //   const response = await apiHandler.get('/jobs/stats');
-  //   return response.data;
-  // } catch (error) {
-  //   return error;
-  // }
+  // const response = await apiHandler.get('/jobs/stats');
+  // return response.data;
+  return null;
 };
 
 function Stats() {
-  const { defaultStats, monthlyApplications } = useLoaderData();
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => apiHandler.get('/jobs/stats'),
+  });
 
+  if (isLoading) return <h4>Loading...</h4>;
+  if (isError) return <h4>Error...</h4>;
+
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+  const { defaultStats, monthlyApplications } = data.data;
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
-      {monthlyApplications.length && (
+      {monthlyApplications?.length && (
         <ChartsContainer data={monthlyApplications} />
       )}
     </>
