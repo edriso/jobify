d952ea08173778c104450a161a26b3fcc39d6292
edriso <edriso.dev@gmail.com>
2023-11-11 14:35:48 +0200

    use react-query in Stats loader

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -3,23 +3,34 @@ import { useQuery } from '@tanstack/react-query';
 import { StatsContainer, ChartsContainer } from '../components';
 import apiHandler from '../utils/apiHandler';
 
-export const loader = async () => {
-  // const response = await apiHandler.get('/jobs/stats');
-  // return response.data;
+const statsQuery = {
+  queryKey: ['stats'],
+  queryFn: async () => {
+    const response = await apiHandler.get('/jobs/stats');
+    return response.data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  //// loader function can be used without a try-catch block,
+  //// and incase of error, <ErrorElement /> will be displayed
+
+  //// Note: Since useQuery is a hook, it cannot be utilized here in the loader,
+  //// as hooks can only be called in a component or a custom hook.
+  //// and that's why we always set up custom hooks with use and whatever the name
+  await queryClient.ensureQueryData(statsQuery);
   return null;
+  //// We can also use the data returned from here, but opting for this approach
+  //// might lead to the loss of other benefits provided by react-query.
+  // const data = await queryClient.ensureQueryData(statsQuery);
+  // return data;
 };
 
 function Stats() {
-  const { isLoading, isError, data } = useQuery({
-    queryKey: ['stats'],
-    queryFn: () => apiHandler.get('/jobs/stats'),
-  });
-
-  if (isLoading) return <h4>Loading...</h4>;
-  if (isError) return <h4>Error...</h4>;
+  const { data } = useQuery(statsQuery);
 
   // const { defaultStats, monthlyApplications } = useLoaderData();
-  const { defaultStats, monthlyApplications } = data.data;
+  const { defaultStats, monthlyApplications } = data;
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
