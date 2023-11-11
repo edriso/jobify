235917f9d31ee59c694ca37360d99b353f674aab
edriso <edriso.dev@gmail.com>
2023-11-11 23:28:53 +0200

    update the limit in rateLimiter

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 const rateLimiter = ({ windowMs, limit, message } = {}) => {
   return rateLimit({
     windowMs: windowMs || 15 * 60 * 1000,
-    max: limit || 70,
+    max: limit || 100,
     message: {
       message: message || 'IP rate limit exceeded, retry in 15 minutes.',
     },
