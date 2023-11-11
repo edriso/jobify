17f364332080eabe54fbd19773e094430052935c
edriso <edriso.dev@gmail.com>
2023-11-11 13:40:09 +0200

    create  ErrorElement component

---
+++ client/src/components/ErrorElement.jsx
@@ -0,0 +1,8 @@
+// import { useRouteError } from 'react-router-dom';
+
+function ErrorElement() {
+  //   const error = useRouteError();
+  //   console.log(error);
+  return <h4>There was an error...</h4>;
+}
+export default ErrorElement;
